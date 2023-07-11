728bc8238845a3ed0d0d1576ccea1d49b38b6104
yangximin <yangximin@maoye.cn>
2023-07-11 17:13:08 +0800

    second commit

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 import App from './App'
+import {$http} from '@escook/request-miniprogram'
+uni.$http = $http
+$http.baseUrl = "https://www.uniav.com"
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:"数据加载中"
+	})
+}
+$http.afterRequest = function(options){
+	uni.hideLoading()
+}
 
 // #ifndef VUE3
 import Vue from 'vue'
