81a1ac14550cd72c194eab6d2f340cb2ab1a27c5
yangximin <yangximin@maoye.cn>
2023-07-11 18:09:41 +0800

    home

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 import App from './App'
-import {$http} from '@escook/request-miniprogram'
+import {
+	$http
+} from '@escook/request-miniprogram'
 uni.$http = $http
-$http.baseUrl = "https://www.uniav.com"
-$http.beforeRequest = function(options){
+$http.baseUrl = "https://www.uinav.com"
+$http.beforeRequest = function(options) {
 	uni.showLoading({
-		title:"数据加载中"
+		title: "数据加载中"
 	})
 }
-$http.afterRequest = function(options){
+$http.afterRequest = function(options) {
 	uni.hideLoading()
+}
+
+uni.$showMsg = function(title = "数据加载失败！", duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
 }
 
 // #ifndef VUE3
@@ -17,17 +27,19 @@ import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-  ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,15 @@
 <template>
 	<view>
-		
+		<swiper class="swiper-view" :indicator-dots="true" autoplay="true" interval="3000" duration="1000"
+			circular="true">
+			<swiper-item v-for="(item,i) in swiperList" :key="i">
+				<navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id">
+					<image :src="item.image_src"></image>
+				</navigator>
+
+			</swiper-item>
+		</swiper>
+
 	</view>
 </template>
 
@@ -8,12 +17,39 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList: [],
 			};
+		},
+		onLoad() {
+			this.getSwiperList()
+		},
+		methods: {
+			async getSwiperList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				if (res.meta.status != 200) {
+					uni.$showMsg()
+				} else {
+					this.swiperList = res.message
+					uni.$showMsg({
+						title: "请求成功",
+					})
+				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	.swiper-view {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			height: 100%;
+			width: 100%;
+
+		}
+	}
 </style>
